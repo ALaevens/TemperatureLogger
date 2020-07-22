f5d066085ca0b4a5fb035a34b9109762bc608523
Alexander Laevens <alaevens@ualberta.ca>
2020-07-22 18:28:18 -0500

    Clean up debug lines, and add a few comments

--- templogger.py
+++ templogger.py
@@ -15,37 +15,39 @@ def createSheet():
 	return sheet
 
 def main():
-	sheet = createSheet()
+	sheet = createSheet() # retrieve reference to the sheet
 
-	sensor = None
-	sensor = W1ThermSensor()
-
-	tCurrent = datetime.now()
-
+	sensor = W1ThermSensor() # Open the temperature sensor
 
 	running = True
 	while running:
 		try:
 			tCurrent = datetime.now()
+
+			# Using the current system time, calculate the time of the next 10 minute interval
 			tNext = tCurrent.replace(microsecond = 0, second = 0, minute = 0) + timedelta(minutes = 10) * (tCurrent.minute // 10 + 1)
 
-			while tCurrent < tNext and running:
+			# Wait until the next measurement should be read
+			while tCurrent <= tNext and running:
 				tCurrent = datetime.now()
 				time.sleep(0.2)
 
 			temp = sensor.get_temperature()
-			print("Current Temperature:", temp, "deg Celcius")
 			timeString = datetime.now().strftime("%Y/%m/%d %H:%M")
+			print(f"Temperature at [{timeString}]: {temp} deg Celcius")
+			
+			# Upload time and temperature to google sheets
 			row = [timeString, temp]
-			print(row)
 			sheet.append_row(row, value_input_option='USER_ENTERED')
 		except KeyboardInterrupt:
 			print("Quitting")
 			running = False
-		except:
+		except Exception as e:
 			print("Closing due to error :(")
 			running = False
-
+			print(repr(e))
+			print(e)
+			print(e.args)
 
 if __name__ == "__main__":
 	main()
